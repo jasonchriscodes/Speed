070331286bdcdc07bd1ed0bd536a05b614b0fedf
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-09-09 15:11:19 +1200

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import css from './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
