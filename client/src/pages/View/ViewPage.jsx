c330c23c7c3bf7a8dbc10cb733023915d4e5313c
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-09-23 08:04:49 +1200

    chore: add dummy data

--- client/src/pages/View/ViewPage.jsx
+++ client/src/pages/View/ViewPage.jsx
@@ -3,7 +3,68 @@ import MaterialTable from "material-table";
 import { useState } from "react";
 
 const ViewPage = () => {
-  const [tableData, setTableData] = useState([]);
+  const [tableData, setTableData] = useState([
+    {
+      id: "1",
+      title:
+        "An experimental evaluation of test driven development vs. test-last development with industry professionals",
+      authors: "Munir, H., Wnuk, K., Petersen, K., Moayyed, M.",
+      source: "EASE",
+      pubyear: "2014",
+      doi: "https://doi.org/10.1145/2601248.2601267",
+      claim: "code quality improvement",
+      evidence: "strong support",
+      practice: "TDD",
+    },
+    {
+      _id: "2",
+      title:
+        "An experimental evaluation of test driven development vs. test-last development with industry professionals",
+      authors: "Munir, H., Wnuk, K., Petersen, K., Moayyed, M.",
+      source: "EASE",
+      pubyear: "2014",
+      doi: "https://doi.org/10.1145/2601248.2601267",
+      claim: "product quality improvement",
+      evidence: "weak support",
+      practice: "BDD",
+    },
+    {
+      _id: "2",
+      title:
+        "An experimental evaluation of test driven development vs. test-last development with industry professionals",
+      authors: "Munir, H., Wnuk, K., Petersen, K., Moayyed, M.",
+      source: "EASE",
+      pubyear: "2014",
+      doi: "https://doi.org/10.1145/2601248.2601267",
+      claim: "product quality improvement",
+      evidence: "weak support",
+      practice: "BDD",
+    },
+    {
+      _id: "3",
+      title:
+        "Realizing quality improvement through test driven development: results and experiences of four industrial teams",
+      authors: "Nagappan, N., Maximilien, E. M., Bhat, T., Williams, L.",
+      source: " Empirical Software Engineering, 13(3), 289–302",
+      pubyear: "2008",
+      doi: "https://doi.org/10.1007/s10664-008-9062-z",
+      claim: "product quality improvement",
+      evidence: "weak support",
+      practice: "ATDD",
+    },
+    {
+      _id: "4",
+      title:
+        "Does Test-Driven Development Really Improve Software Design Quality?",
+      authors: "Janzen, D. S.",
+      source: "Software, IEEE, 25(2) 77-84",
+      pubyear: "2008",
+      doi: "",
+      claim: "code quality improvement",
+      evidence: "strong support",
+      practice: "TDD",
+    },
+  ]);
   const columns = [
     { title: "title", field: "title" },
     { title: "authors", field: "authors" },
@@ -21,6 +82,6 @@ const ViewPage = () => {
       <MaterialTable columns={columns} data={tableData} />
     </div>
   );
-}
+};
 
 export default ViewPage;
