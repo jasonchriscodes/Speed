12b1f3459b39320b173a5ea3ec97fdac46397791
Ajit Singh <Ajit_Singh_1@outlook.com>
2022-09-26 11:03:20 +1300

    changes variable names and moves the model folder into the backend folder

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('../config/db');
+
 const app = express();
 const port = process.env.PORT || 8888;
 
 // routes
-const articles = require('./routes/api/Repository');
+const AppRoutes = require('./routes/api/Repository');
 
 // cors config - allow same origin
 const corsOptions = {
@@ -19,19 +20,23 @@ app.use(cors(corsOptions));
 // init body parser
 app.use(express.json());
 
+app.use((request, response, next) => {
+  console.log(request.path, request.method, request.body);
+  next();
+});
+
 // use routes here
-app.use('/api/articles', articles);
+app.use('/api/articles', AppRoutes);
 
 // Connect to Mongo Atlas
 connectDB();
 
 if (process.env.NODE_ENV === 'production') {
   // serve front-end client from build folder
-  app.use(express.static(__dirname+'/../client/build'));
-  app.get('*', (req, res) =>{
-    res.sendFile(__dirname+'/../client/build/index.html')
+  app.use(express.static(__dirname + '/../client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/../client/build/index.html');
   });
-  
 } else {
   app.get('/', (req, res) => res.send(`API running on port ${port}`));
 }
