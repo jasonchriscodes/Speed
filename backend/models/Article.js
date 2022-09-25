3a31de1ddec0901b69915e594fdf99cef5f13d9b
Ajit Singh <Ajit_Singh_1@outlook.com>
2022-09-26 10:44:53 +1300

    adds Article schema

---
+++ backend/models/Article.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-multi-assign */
+/* eslint-disable no-undef */
+const mongoose = require('mongoose');
+
+const ArticleSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  authors: {
+    type: String,
+    required: true,
+  },
+  source: {
+    type: String,
+    required: true,
+  },
+  pubyear: {
+    type: String,
+    required: true,
+  },
+  doi: {
+    type: String,
+  },
+  claim: {
+    type: String,
+    required: true,
+  },
+  evidence: {
+    type: String,
+    required: true,
+  },
+  practice: {
+    type: String,
+    required: true,
+  },
+});
+
+const Article = mongoose.model('Article', ArticleSchema);
+module.exports = Article;
