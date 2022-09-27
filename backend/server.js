174bbcdc00f094c761ecfdaf187cb25d03a681e4
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-09-27 15:05:46 +1300

    chore: refactor root folder

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('../config/db');
+const connectDB = require('./config/db');
 const app = express();
 const port = process.env.PORT || 8888;
 
