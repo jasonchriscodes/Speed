6462b1b1dd10e61aa712a9c4174c14b3aaccd9fc
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-10-02 18:21:58 +1300

    fix: routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 // import css from './App.module.css';
-import { BrowserRouter, Route } from 'react-router-dom';
-import ViewPage from "./pages/View/ViewPage";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ViewPage from './pages/View/ViewPage';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Route exact path="/" component={ViewPage} />
-      </div>
+      <Routes>
+        <Route exact path="/" element={<ViewPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
