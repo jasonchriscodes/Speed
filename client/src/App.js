980a6fa3577abc2e63905d7574b4922047c1fdd1
Ajit Singh <Ajit_Singh_1@outlook.com>
2022-10-04 16:05:43 +1300

    modify TableView reference with Table

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 // import css from './App.module.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ViewPage from './pages/View/ViewPage';
+import Table from './pages/View/Table';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<ViewPage />} />
+        <Route exact path="/" element={<Table />} />
       </Routes>
     </BrowserRouter>
   );
