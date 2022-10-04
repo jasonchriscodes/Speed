d38b335806620e2e0bcf96bfa00861b6d3b7d67f
Ajit Singh <Ajit_Singh_1@outlook.com>
2022-10-04 18:19:17 +1300

    moves the file into a new folder called components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 // import css from './App.module.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Table from './pages/View/Table';
+// import Table from './components/Table';
+import HomeView from './pages/View/HomeView';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Table />} />
+        <Route exact path="/" element={<HomeView />} />
       </Routes>
     </BrowserRouter>
   );
