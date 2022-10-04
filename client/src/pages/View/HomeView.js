3a6eae1fe4a0ab882ec782f881d567f734c7eaff
Ajit Singh <Ajit_Singh_1@outlook.com>
2022-10-04 18:20:52 +1300

    adds HomeView.js file as a home view

---
+++ client/src/pages/View/HomeView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Table from '../../components/Table.js';
+
+const HomeView = () => {
+    return (
+        <React.Fragment>
+            <Table/>
+        </React.Fragment>
+    );
+};
+
+export default HomeView; 
