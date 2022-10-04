e0fca7745e42bc0019189caefe18e44f3ee5b5b9
Ajit Singh <Ajit_Singh_1@outlook.com>
2022-10-05 12:08:00 +1300

    add test for request response of 404

--- backend/tests/routes.test.js
+++ backend/tests/routes.test.js
@@ -12,6 +12,11 @@ describe('GET Endpoints', () => {
 
     // check the response code of the response
     expect(response.statusCode).toEqual(200); // the response code should be 200
+  });
+
+  it('GET Articles Returns Status Code 200', async () => {
+    // get the response from the path /all-articles
+    const response = await request(BASE_URL).get('/all-articles');
     expect(response.statusCode).not.toEqual(404); // the response code should not be 404
   });
 });
