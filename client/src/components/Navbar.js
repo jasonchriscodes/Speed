fd629ec32081a086aeadfe16c352e206a1d008c5
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-10-08 16:19:20 +1300

    chore: add navbar styles

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { useState } from 'react';
+import { close, logo, menu } from './../assets';
+import { navLinks } from './../constans';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="speed" className="w-[124px] h-[32px]" />
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white mr-10`}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 };
 
