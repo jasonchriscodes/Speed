f46c86b084ac9477e4a5871a4c0e6f23524719ab
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-10-08 14:40:31 +1300

    chore: add padding in multi devices

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import "./style-components/navigation.css";
+import styles from "./style-components/styles";
 
 const Navigation = () => (
   <div className="bg-primary w-full overflow-hidden">
-    <h1>Hello World</h1>
+    <div className={`${styles.paddingX}`}>
+
+    </div>
   </div>
 );
 
