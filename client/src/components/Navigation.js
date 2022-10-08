aa29c84e3a9c321bafe24eee9dae69a7479b0705
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-10-08 14:45:18 +1300

    chore: add hero section

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -5,9 +5,11 @@ import styles from './style-components/styles';
 const Navigation = () => (
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-       Navbar
-      </div>
+      <div className={`${styles.boxWidth}`}>Navbar</div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>Hero</div>
     </div>
   </div>
 );
