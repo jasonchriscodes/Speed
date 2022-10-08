ac1bb747ac3e6634f534d08e6158b25b1f2ad707
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-10-08 16:49:23 +1300

    chore: add animation box mobile view toggle

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { useState } from 'react';
-import { close, logo, menu } from './../assets';
-import { navLinks } from './../constans';
+import React from 'react'
+import { useState } from 'react'
+import { close, logo, menu } from './../assets'
+import { navLinks } from './../constans'
 
 const Navbar = () => {
-  const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(false)
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="speed" className="w-[124px] h-[32px]" />
@@ -29,9 +29,15 @@ const Navbar = () => {
           className="w-[28px] h-[28px] object-contain"
           onClick={() => setToggle((prev) => !prev)}
         />
+
+        <div
+          className={`${
+            toggle ? 'flex' : 'hidden'
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        ></div>
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
