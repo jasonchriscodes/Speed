12b1e2b159475d32217c27684e6dfbb3efd5aaa2
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-10-08 15:01:22 +1300

    chore: add components to navigation

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './style-components/navigation.css';
 import styles from './style-components/styles';
+import {Navbar, Hero, Footer} from "./index";
 
 const Navigation = () => (
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>Navbar</div>
+      <div className={`${styles.boxWidth}`}><Navbar/></div>
     </div>
 
     <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>Hero</div>
+      <div className={`${styles.boxWidth}`}><Hero/></div>
     </div>
 
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>Footer</div>
+      <div className={`${styles.boxWidth}`}><Footer/></div>
     </div>
   </div>
 );
