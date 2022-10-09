a9bd2f1e2f624a9d87af56a7a2300c0d1bbd1efb
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-10-09 16:44:26 +1300

    style: format tabs

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './style-components/styles';
-import GetStarted from "./GetStarted";
+import GetStarted from './GetStarted';
 import { robot } from '../assets';
 
 const Hero = () => (
@@ -18,18 +18,24 @@ const Hero = () => (
         </div>
       </div>
       <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">
-          Data Management.
-        </h1>
-        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Our team of experts uses a methodology to identify the data storage most likely to fit your needs. 
-We record articles digitally and saved for future access.</p>
-
+        Data Management.
+      </h1>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Our team of experts uses a methodology to identify the data storage most
+        likely to fit your needs. We record articles digitally and saved for
+        future access.
+      </p>
     </div>
-<div>
-  <img src={robot} alt="robot" className='w-[100%] h-[100%] relative z-[5]'/>
-  <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'/>
-  <div className='absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient'/>
-  <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient'/>
-</div>
+    <div>
+      <img
+        src={robot}
+        alt="robot"
+        className="w-[100%] h-[100%] relative z-[5]"
+      />
+      <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+      <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient" />
+      <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+    </div>
   </section>
 );
 export default Hero;
