bdbf564a9bee0154cafdcdcaa7534ca48622c3d1
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-10-09 17:03:12 +1300

    chore: add footer copyright

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import styles from './style-components/styles';
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  );
-};
+const Footer = () => (
+  <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+    <div className='w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3e45]'>
+      <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
+        2022 SPEED. All Rights Reserved.
+      </p>
+    </div>
+  </section>
+);
 
 export default Footer;
