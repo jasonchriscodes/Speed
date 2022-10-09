81e3c86cfc55af992bbc475ba9c5d838616da9a5
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-10-09 15:22:13 +1300

    chore: create round border

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './style-components/styles';
+import GetStarted from "./GetStarted";
 
 const Hero = () => (
   <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
@@ -7,11 +8,14 @@ const Hero = () => (
       className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
     >
       <div className="flex flex-row justify-between items-center w-full">
-        <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>
+        <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
           The Next <br className="sm:block hidden" />
-          <span className="text-gradient">Generation{" "}</span>
+          <span className="text-gradient">Generation </span>
           Data Management
         </h1>
+        <div className="ss:flex hidden md:mr-4 mr-0">
+          <GetStarted />
+        </div>
       </div>
     </div>
   </section>
