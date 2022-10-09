89e55b6bf44d4e4859682aa3f7ad310b13abc5e1
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-10-09 15:27:01 +1300

    chore: add margin right

--- client/src/components/GetStarted.js
+++ client/src/components/GetStarted.js
@@ -10,7 +10,7 @@ const GetStarted = () => (
       className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
     >
       <div className={`${styles.flexStart} flex-row`}>
-        <p className='font-poppins font-medium text-[18px] leading-[23px]'>
+        <p className='font-poppins font-medium text-[18px] leading-[23px] mr-2'>
           <span className='text-gradient'>Get</span>
         </p>
           <img src={arrowUp} alt="arrow" className="w-[23px] h-[23px] object-contain"/>
