4e3b66dc271e4e8da19769ede47193ea6a13c5cd
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-10-09 15:06:06 +1300

    chore: add hero title

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import styles from './style-components/styles';
 
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  );
-};
-
+const Hero = () => (
+  <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+    <div
+      className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
+    >
+      <div className="flex flex-row justify-between items-center w-full">
+        <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>
+          The Next <br className="sm:block hidden" />
+          <span className="text-gradient">Generation{" "}</span>
+          Data Management
+        </h1>
+      </div>
+    </div>
+  </section>
+);
 export default Hero;
