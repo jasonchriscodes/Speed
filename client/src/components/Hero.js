1c7864fbdb5fc79f18222f74fd97fb041451f29f
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-10-09 16:43:37 +1300

    chore: add gradient

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -26,6 +26,9 @@ We record articles digitally and saved for future access.</p>
     </div>
 <div>
   <img src={robot} alt="robot" className='w-[100%] h-[100%] relative z-[5]'/>
+  <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'/>
+  <div className='absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient'/>
+  <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient'/>
 </div>
   </section>
 );
