abf2701f8a64b837f38c12e815ca4a269ba02fee
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-10-09 15:26:00 +1300

    fix: arrow position

--- client/src/components/GetStarted.js
+++ client/src/components/GetStarted.js
@@ -12,8 +12,8 @@ const GetStarted = () => (
       <div className={`${styles.flexStart} flex-row`}>
         <p className='font-poppins font-medium text-[18px] leading-[23px]'>
           <span className='text-gradient'>Get</span>
-          <img src={arrowUp} alt="arrow" className="w-[23px] h-[23px] object-contain"/>
         </p>
+          <img src={arrowUp} alt="arrow" className="w-[23px] h-[23px] object-contain"/>
       </div>
       <p className='font-poppins font-medium text-[18px] leading-[23px]'>
           <span className='text-gradient'>Started</span>
