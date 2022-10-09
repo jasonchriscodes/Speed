7da0d8ebc3999cd04fb567205d829a911e888f94
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-10-09 15:25:15 +1300

    chore: add another text gradient

--- client/src/components/GetStarted.js
+++ client/src/components/GetStarted.js
@@ -15,6 +15,9 @@ const GetStarted = () => (
           <img src={arrowUp} alt="arrow" className="w-[23px] h-[23px] object-contain"/>
         </p>
       </div>
+      <p className='font-poppins font-medium text-[18px] leading-[23px]'>
+          <span className='text-gradient'>Started</span>
+        </p>
     </div>
   </div>
 );
