9d94878966c9475d98a53bc6e19dbaf991500642
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-10-09 15:22:56 +1300

    chore: add text gradient

--- client/src/components/GetStarted.js
+++ client/src/components/GetStarted.js
@@ -8,6 +8,11 @@ const GetStarted = () => (
     <div
       className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
     >
+      <div className={`${styles.flexStart} flex-row`}>
+        <p className='font-poppins font-medium text-[18px] leading-[23px]'>
+          <span className='text-gradient'>Get</span>
+        </p>
+      </div>
     </div>
   </div>
 );
