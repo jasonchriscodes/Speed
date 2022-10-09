b39552baa6abea86c4d510115705ac83c8be3e2d
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-10-09 17:39:42 +1300

    chore: add about, submit, view pages

---
+++ client/src/components/View/View.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const View = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '90vh'
+      }}
+    >
+      <h1>View</h1>
+    </div>
+  );
+};
+
+export default View;
