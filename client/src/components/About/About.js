b224d7ca76407c8735bd6f5a59b74167b0a980ba
jasonchriscodes <jasonchriscodes1234@gmail.com>
2022-10-10 22:40:18 +0700

    fix: eslint and deployment

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -2,18 +2,24 @@ import React from 'react';
 import { features } from '../Navigation/constans';
 import styles, { layout } from './../Navigation/style-components/styles';
 
-const FeatureCard = ({icon, title, content, index}) =>(
-  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length-1 ? "mb-6" : "mb-0"} feature-card`}>
-    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
-      <img src={icon} alt="icon" className='w-[50%] h-[50%] object-contain'/>
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div
+    className={`flex flex-row p-6 rounded-[20px] ${
+      index !== features.length - 1 ? 'mb-6' : 'mb-0'
+    } feature-card`}
+  >
+    <div
+      className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
+    >
+      <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
     </div>
-    <div className='flex-1 flex flex-col ml-3'>
-    <h4 className='font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1'>
-      {title}
-    </h4>
-    <p className='font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1'>
-      {content}
-    </p>
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1">
+        {content}
+      </p>
     </div>
   </div>
 );
@@ -31,9 +37,9 @@ const About = () => {
         </p>
       </div>
       <div className={`${layout.sectionImg} flex-col`}>
-{features.map((feature, index) => (
-  <FeatureCard key={feature.id} {...feature} index={index}/>
-))}
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index} />
+        ))}
       </div>
     </section>
   );
