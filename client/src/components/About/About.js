3ecfe4ad38922b8d519e190cf027bac50d338014
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-10-10 15:07:29 +1300

    chore: add paragraph

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -6,6 +6,12 @@ const About = () => {
     <section id="features" className={layout.section}>
       <div className={layout.sectionInfo}>
         <h2 className={styles.heading2}>About Us</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          The SPEED team is passionate about making the complex simple for
+          customers. We give our customers control and confidence that their
+          data can be accessed quickly. SPEED is a highly scalable platform
+          equally suitable for storing articles data.
+        </p>
       </div>
     </section>
   );
