177f3187eb3b48216c599a42dbab5d30d561ab58
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-10-10 15:41:13 +1300

    chore: add icon styling

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -3,9 +3,9 @@ import { features } from '../Navigation/constans';
 import styles, { layout } from './../Navigation/style-components/styles';
 
 const FeatureCard = ({icon, title, content, index}) =>(
-  <div>
-    <div>
-      <img src={icon} alt="icon"/>
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length-1 ? "mb-6" : "mb-0"} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt="icon" className='w-[50%] h-[50%] object-contain'/>
     </div>
   </div>
 );
