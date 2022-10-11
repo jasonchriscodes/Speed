f4a00e46873197c407a4eb92305d1c90661e5560
Ajit Singh <Ajit_Singh_1@outlook.com>
2022-10-11 16:18:25 +1300

    adds functionality to POST new article submission

--- client/src/components/Submit/Submit.js
+++ client/src/components/Submit/Submit.js
@@ -1,8 +1,58 @@
-import React from "react";
-
+import React, { useState } from "react";
 import { Card, CardContent, Grid, TextField, Button} from '@material-ui/core';
+import Axios from 'axios';
+
+const URL_POST = 'http://localhost:8888/api/articles/post-new-submit-article';
 
 const Submit = () => {
+    const [title, setTitle] = useState('');
+    const [authors, setAuthors] = useState('');
+    const [journalName, setJournalName] = useState('');
+    const [volume, setVolume] = useState('');
+    const [number, setNumber] = useState('');
+    const [pages, setPages] = useState('');
+    const [doi, setDOI] = useState('');
+    const [pubyear, setPubyear] = useState('');
+
+    const [titleError, setTitleError] = useState('');
+    const [authorsError, setAuthorsError] = useState('');
+    const [journalNameError, setJournalNameError] = useState('');
+    const [volumeError, setVolumeError] = useState('');
+    const [numberError, setNumberError] = useState('');
+    const [pagesError, setPagesError] = useState('');
+    const [doiError, setDOIError] = useState('');
+    const [pubyearError, setPubyearError] = useState('');
+
+
+
+    const onTitleChangeHandler = (e) => setTitle(e.target.value);
+    const onAuthorChangeHandler = (e) => setAuthors(e.target.value);
+    const onJournalNameChangeHandler = (e) => setJournalName(e.target.value);
+    const onVolumeChangeHandler = (e) => setVolume(e.target.value);
+    const onNumberChangeHandler = (e) => setNumber(e.target.value);
+    const onPagesChangeHandler = (e) => setPages(e.target.value);
+    const onDOIChangeHandler = (e) => setDOI(e.target.value);
+    const onPubYearChangeHandler = (e) => setPubyear(e.target.value);
+
+    const postNewSubmitArticle = () => {
+        Axios.post(URL_POST, {
+            title: title,
+            authors: authors,
+            journalName: journalName,
+            volume: volume,
+            number: number,
+            pages: pages,
+            doi: doi,
+            pubyear: pubyear,
+        }).then((response) => {
+            console.log(response);
+        })
+          .catch((error) => console.log(error));          
+
+        alert('New Article Submitted');
+        
+    };
+
     return (
         <div className="App">
             <Card style={{maxWidth:450, margin:"0 auto", padding:"20px 5px"}}>
@@ -10,31 +60,31 @@ const Submit = () => {
                     <form>
                     <Grid container spacing={1} >
                         <Grid xs={12} item>
-                            <TextField label="Title" placeholder="Enter title" variant="outlined" fullWidth required/>
+                            <TextField onChange={onTitleChangeHandler}  label="Title" placeholder={title} variant="outlined" fullWidth required/>
                         </Grid>
                         <Grid xs={12} item>
-                            <TextField label="Author" placeholder="Enter author" variant="outlined" fullWidth required/>
+                            <TextField onChange={onAuthorChangeHandler} label="Author" placeholder="Enter author" variant="outlined" fullWidth required/>
                         </Grid>
                         <Grid xs={12} item>
-                            <TextField label="Journal Name" placeholder="Enter journal name" variant="outlined" fullWidth required/>
+                            <TextField onChange={onJournalNameChangeHandler} label="Journal Name" placeholder="Enter journal name" variant="outlined" fullWidth required/>
                         </Grid>
                         <Grid xs={12} item>
-                            <TextField label="Volume" placeholder="Enter volume" variant="outlined" fullWidth required/>
+                            <TextField onChange={onVolumeChangeHandler} label="Volume" placeholder="Enter volume" variant="outlined" fullWidth required/>
                         </Grid>
                         <Grid xs={12} item>
-                            <TextField label="Number" placeholder="Enter number" variant="outlined" fullWidth required/>
+                            <TextField onChange={onNumberChangeHandler} label="Number" placeholder="Enter number" variant="outlined" fullWidth required/>
                         </Grid>
                         <Grid xs={12} item>
-                            <TextField label="Pages" placeholder="Enter pages" variant="outlined" fullWidth required/>
+                            <TextField onChange={onPagesChangeHandler} label="Pages" placeholder="Enter pages" variant="outlined" fullWidth required/>
                         </Grid>
                         <Grid xs={12} item>
-                            <TextField label="DOI" placeholder="Enter DOI" variant="outlined" fullWidth required/>
+                            <TextField onChange={onDOIChangeHandler} label="DOI" placeholder="Enter DOI" variant="outlined" fullWidth required/>
                         </Grid>
                         <Grid xs={12} item>
-                            <TextField InputLabelProps={{ shrink: true }} type = "date" label="Publication date" placeholder="Enter publication date" variant="outlined" fullWidth required/>
+                            <TextField onChange={onPubYearChangeHandler} InputLabelProps={{ shrink: true }} type = "date" label="Publication date" placeholder="Enter publication date" variant="outlined" fullWidth required/>
                         </Grid>
                         <Grid xs={12} item>
-                            <Button variant="contained" color="primary" fullWidth>Submit</Button>
+                            <Button onClick={postNewSubmitArticle} variant="contained" color="primary" fullWidth>Submit</Button>
                         </Grid>
                     </Grid>
                     </form>
