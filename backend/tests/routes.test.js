7d4b1ef7bff6bb392ce6b82759b7e68ff2ba0204
jasonchriscodes <jasonchriscodes1234@gmail.com>
2022-10-19 01:00:11 +0700

    fix: set exceed time

--- backend/tests/routes.test.js
+++ backend/tests/routes.test.js
@@ -8,17 +8,18 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 describe('GET Endpoints', () => {
+  console.log('process.env.BASE_URL : ', process.env.BASE_URL);
   it('GET Articles Returns Status Code 200', async () => {
     // get the response from the path /all-articles
     const response = await request(process.env.BASE_URL).get('/all-articles');
 
     // check the response code of the response
     expect(response.statusCode).toEqual(200); // the response code should be 200
-  });
+  }, 10000);
 
   it('GET Articles Returns Status Code 200', async () => {
     // get the response from the path /all-articles
     const response = await request(process.env.BASE_URL).get('/all-articles');
     expect(response.statusCode).not.toEqual(404); // the response code should not be 404
-  });
+  }, 10000);
 });
