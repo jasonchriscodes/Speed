f5bdddb16b6eec616bb68ba5762f6eb6d3cd5b59
jasonchriscodes <jasonchriscodes1234@gmail.com>
2022-10-20 00:25:54 +0700

    fix: add exceed timeout

--- backend/tests/routes.test.js
+++ backend/tests/routes.test.js
@@ -15,11 +15,11 @@ describe('GET Endpoints', () => {
 
     // check the response code of the response
     expect(response.statusCode).toEqual(200); // the response code should be 200
-  }, 10000);
+  }, 30000);
 
   it('GET Articles Returns Status Code 200', async () => {
     // get the response from the path /all-articles
     const response = await request(process.env.BASE_URL).get('/all-articles');
     expect(response.statusCode).not.toEqual(404); // the response code should not be 404
-  }, 10000);
+  }, 30000);
 });
